e55a1fdadd5d8bcf5910b2c47c7cc0ff1532669f
Jeffry Gonzalez <jeff@hypertheory.com>
2019-09-24 18:28:10 +0000

    error on post

--- src/holidays/holidays.controller.ts
+++ src/holidays/holidays.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpException } from '@nestjs/common';
 import { HolidaysService } from './holidays.service';
 import { HolidayPost } from './models';
 
@@ -17,6 +17,9 @@ export class HolidaysController {
 
     @Post()
     async addHoliday(@Body() holiday: HolidayPost) {
+        if (holiday.name.toLowerCase() === 'president\'s day') {
+            throw new HttpException('Nate hates that holiday.', 400);
+        }
         const result = this.service.addOne(holiday);
         return new Promise((res) => {
             setTimeout(() => res(result), 3000);
